7227cf3629a8880ef3782d995a5dcb8e310e1a49
Kundan559 <122978514+Kundan559@users.noreply.github.com>
2025-03-25 11:30:59 +0000

    comparison

---
+++ .devcontainer/01_basics/1_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
+
+
+// this type of conversion can be confused
+//=== 
+console.log("2"===2);//not are same
+
+
+
+
+
+
+
